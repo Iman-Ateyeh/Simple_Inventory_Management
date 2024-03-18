e37dd702ae85a1960a69cd9dc62d922a23c2cd69
Eman Ateyeh <iman.ateyeh@students.alquds.edu>
2024-03-18 22:43:24 +0200

    Test
    يا رب تزبط تعبت

---
+++ InventoryManagement_exercise6_version2/InventoryManagement_exercise6_version2/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Inventory
+    {
+        private List<Product> products;
+        private int nextId;
+
+        public void AddProduct(string name, double price, int quantity)
+        {
+            products.Add(new Product(nextId, name, price, quantity));
+            Console.WriteLine($"{quantity} {name} were added successfully.");
+            nextId++;
+        }
+
+
+
+    }
+}
