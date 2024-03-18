2b4b8af28c63db80f3be5760448ed16c71acbcf7
Eman Ateyeh <iman.ateyeh@students.alquds.edu>
2024-03-18 17:04:01 +0200

    Test

---
+++ InventoryManagement_exercise6_version2/InventoryManagement_exercise6_version2/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Product
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private double Price { get; set; }
+        private int Quantity { get; set; }
+        private int test; 
+       
+
+
+        public  Product(int id, string name, double price, int quantity)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price; 
+            this.Quantity = quantity;
+        }
+
+    }
+}
